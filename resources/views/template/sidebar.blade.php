ffafd62d8757f99bac565e82fb24b08a024e038f
cbettyn <betty.novaria@gmail.com>
2020-07-02 04:32:35 +0700

    first commit

---
+++ resources/views/template/sidebar.blade.php
@@ -0,0 +1,39 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+  <!-- Brand Logo -->
+  <a href="../../index3.html" class="brand-link">
+    <img src="../../dist/img/AdminLTELogo.png"
+         alt="AdminLTE Logo"
+         class="brand-image img-circle elevation-3"
+         style="opacity: .8">
+    <span class="brand-text font-weight-light">Sanbercode</span>
+  </a>
+
+  <!-- Sidebar -->
+  <div class="sidebar">
+
+    <!-- Sidebar Menu -->
+    <nav class="mt-2">
+      <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+        <!-- Add icons to the links using the .nav-icon class
+             with font-awesome or any other icon font library -->
+        <li class="nav-item">
+          <a href="/" class="nav-link">
+            <i class="nav-icon fas fa-th"></i>
+            <p>
+              Main
+            </p>
+          </a>
+        </li>
+        <li class="nav-item has-treeview">
+          <a href="/data-tables" class="nav-link">
+            <i class="nav-icon fas fa-table"></i>
+            <p>
+              DataTables
+            </p>
+          </a>
+      </ul>
+    </nav>
+    <!-- /.sidebar-menu -->
+  </div>
+  <!-- /.sidebar -->
+</aside>
